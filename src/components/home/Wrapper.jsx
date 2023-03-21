7c3b3250e809bb65104e47d877a95c14bff6aac6
vinaydeep26 <vinaydeep26@gmail.com>
2023-03-21 16:47:36 -0400

    new file:   public/assets/blog/blog1.jpg 	new file:   public/assets/blog/blog2.jpg 	new file:   public/assets/blog/blog3.jpg 	new file:   public/assets/work/work1.jpg 	new file:   public/assets/work/work2.jpg 	new file:   public/assets/work/work3.jpg 	new file:   public/assets/work/work4.jpg 	new file:   public/assets/work/work5.jpg 	new file:   public/assets/work/work6.jpg 	modified:   src/App.css 	new file:   src/components/home/Skill.jsx 	modified:   src/components/home/Wrapper.jsx 	new file:   src/components/home/work/Data.js 	new file:   src/components/home/work/Work.jsx 	new file:   src/components/home/work/blog/Blog.jsx 	new file:   src/components/home/work/blog/Blogdata.js 	modified:   src/components/pageContent/HomePages.jsx

--- src/components/home/Wrapper.jsx
+++ src/components/home/Wrapper.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
 const Wrapper = () => {
+    const data = [
+        {
+          title: "LOOKING FOR EXCLUSIVE SERVICES?",
+          heading: "Get The Best For Your Business",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        },
+      ]
   return (
     <>
     <section className="branding wrapper">
         <div className="container">
-            {DataTransfer.map((val) => {
-                
+            {data.map((val) => {
+                return (
+                <div className="box">
+                    <h3>{val.title}</h3>
+                    <h2>{val.heading}</h2>
+                    <p>{val.desc}</p>
+                    <button className="primary-btn"> Contact Me</button>
+                </div>
 
+                )
             })}
         </div>
     </section>
--- src/components/pageContent/HomePages.jsx
+++ src/components/pageContent/HomePages.jsx
@@ -3,6 +3,9 @@ import About from '../home/about/About'
 import Branding from '../home/Branding'
 import Home from '../home/Homes/Home'
 import { Service } from '../home/servicess/Service'
+import Skill from '../home/Skill'
+import Blog from '../home/work/blog/Blog'
+import Work from '../home/work/Work'
 import Wrapper from '../home/Wrapper'
 const HomePages = () => {
   return (
@@ -12,6 +15,11 @@ const HomePages = () => {
     <About />
     <Service />
     <Wrapper />
+    <Skill />
+    {/* <WrapperOne /> */}
+    < Work />
+    <Blog />
+
     </>
   ) 
 }
